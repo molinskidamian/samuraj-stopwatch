df54fe5b497ac95db16be415d779ba9dd344e6c9
Damian Molinski <tedeer@gmail.com>
2021-05-05 14:57:39 +0200

    It is not finished. Try create seconds solution with used setTimeout, not the setInterval

--- main.js
+++ main.js
@@ -1,47 +1,58 @@
 const btnStart = document.querySelector('.start');
 const btnReset = document.querySelector('#reset');
-const seconds = document.querySelector('.seconds');
-const miliseconds = document.querySelector('.miliseconds');
+const milisecondsField = document.querySelector('.miliseconds');
+const secondsField = document.querySelector('.seconds');
+const minutesField = document.querySelector('.minutes')
 
-let watchstopCount = false; // Stoped
-let secondsValue = 0;
+let miliseconds = 0;
+let seconds = 0;
+let minutes = 0;
+
 let counter = 0;
 
+const countTime = () => {
+  if(minutes <= 2) {
 
-btnStart.addEventListener('click', () => {
-  const countTime = () => {
-    if (watchstopCount) {
-      counter++;
-      console.log(counter);
-      if(counter === 60){
-        secondsValue++;
-        counter = 0;
-        seconds.textContent = secondsValue < 10 ? (secondsValue = `0${secondsValue}`) : secondsValue;
+    console.log(seconds++);
+    secondsField.textContent = (seconds < 10) ? `0${seconds}` : seconds;
 
-        }
-        miliseconds.textContent = counter < 10 ? counter = `0${counter}` : counter;
+    if(seconds > 3){
+      seconds = 0;
+      minutes++;
+      minutesField.textContent = minutes < 10 ? `0${minutes}` : minutes;
     }
-  };
 
-  //  Change colors buttons
-  if(!watchstopCount) {
-    btnStart.textContent = 'Start';
-    btnStart.classList.add("green");
-    btnStart.classList.remove("red");
-    intervelIndex = setInterval(countTime, 100);
-    watchstopCount = true;
-  } else {
-    btnStart.textContent = 'Stop';
-    btnStart.classList.add('red');
-    btnStart.classList.remove('green');
-    clearInterval(intervelIndex);
-    watchstopCount = false;
+    if(miliseconds === 1000){
+      seconds++;
+      miliseconds = 0;
+      console.log(seconds);
+
+      secondsField.textContent = seconds;
+    }
+
+    // miliseconds++;
+    // // console.log(`ms: ${miliseconds}`);
+
+
+    // if(miliseconds === 1000) {
+    //   seconds++;
+    //   miliseconds = 0;
+    //   milisecondsField.textContent = miliseconds;
+    //   console.log(`[s: ${seconds}]`);
+
+    // }
+
+    // if(seconds === 60) {
+    //   minutes++;
+    //   seconds = 0;
+    //   miliseconds = 0;
+    //   secondsField.textContent = seconds;
+    //   console.log(`[s: ${minutes}]`);
+
+    // }
+
+    setTimeout(countTime, 1000);
   }
+}
 
-  console.log('uruchomienie setinterval');
-
-});
-
-// btnStart.addEventListener('click', () => {
-//   console.log(btnStop)
-// });
+countTime();
