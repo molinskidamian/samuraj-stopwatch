dbc68a07fa7c394128e63014f84af31315d83d1a
Damian Molinski <tedeer@gmail.com>
2021-05-06 16:41:25 +0200

    New idea to resolve

--- main.js
+++ main.js
@@ -1,58 +1,53 @@
 const btnStart = document.querySelector('.start');
 const btnReset = document.querySelector('#reset');
-const milisecondsField = document.querySelector('.miliseconds');
+const miliSecondsField = document.querySelector('.miliseconds');
 const secondsField = document.querySelector('.seconds');
 const minutesField = document.querySelector('.minutes')
 
-let miliseconds = 0;
-let seconds = 0;
-let minutes = 0;
+let activeStopFlagBtn = false;
+let miliSeconds = null;
+let seconds = null;
 
-let counter = 0;
+const changeButton = () => {
 
-const countTime = () => {
-  if(minutes <= 2) {
-
-    console.log(seconds++);
-    secondsField.textContent = (seconds < 10) ? `0${seconds}` : seconds;
-
-    if(seconds > 3){
-      seconds = 0;
-      minutes++;
-      minutesField.textContent = minutes < 10 ? `0${minutes}` : minutes;
-    }
-
-    if(miliseconds === 1000){
-      seconds++;
-      miliseconds = 0;
-      console.log(seconds);
-
-      secondsField.textContent = seconds;
-    }
-
-    // miliseconds++;
-    // // console.log(`ms: ${miliseconds}`);
-
-
-    // if(miliseconds === 1000) {
-    //   seconds++;
-    //   miliseconds = 0;
-    //   milisecondsField.textContent = miliseconds;
-    //   console.log(`[s: ${seconds}]`);
-
-    // }
-
-    // if(seconds === 60) {
-    //   minutes++;
-    //   seconds = 0;
-    //   miliseconds = 0;
-    //   secondsField.textContent = seconds;
-    //   console.log(`[s: ${minutes}]`);
-
-    // }
-
-    setTimeout(countTime, 1000);
+  if(!activeStopFlagBtn) {
+    btnStart.textContent = 'Stop';
+    btnStart.classList.remove('green');
+    btnStart.classList.add('red');
+    activeStopFlagBtn = true;
+  } else {
+    btnStart.textContent = 'Start';
+    btnStart.classList.remove('red');
+    btnStart.classList.add('green');
+    activeStopFlagBtn = false;
   }
+
+  console.log(activeStopFlagBtn);
 }
 
-countTime();
+
+const startCount = () => {
+
+  if(activeStopFlagBtn){
+    ++miliSeconds;
+    miliSecondsField.textContent = miliSeconds < 10 ? `0${miliSeconds}` : miliSeconds;
+    if(miliSeconds === 59){
+      ++seconds;
+      miliSeconds = 0;
+      secondsField.textContent = seconds < 10 ? `0${seconds}` : seconds;
+    }
+    console.log(miliSeconds);
+    timeoutIndex = setTimeout(startCount, 1000);
+  }
+
+
+}
+
+btnStart.addEventListener("click", changeButton);
+btnStart.addEventListener("click", startCount);
+
+
+btnReset.addEventListener('click', function(){
+  console.log(`Wywołano funkcję timeoutIndex`);
+  activeStopFlagBtn = false;
+})
